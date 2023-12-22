6bb110f80336e2403948d279df6cfedaaf92ef70
Faisal Dad Khan <54923395+faisaldadkhan13@users.noreply.github.com>
2023-12-22 14:10:41 +0530

    Revert "Update script.js for Testing"

--- script.js
+++ script.js
@@ -44,16 +44,3 @@ async function run() {
 }
 
 run();
-
-async function detectObjects(model, video) {
-    const model = await cocoSsd.load();
-    const canvas = document.getElementById('canvas');
-    const ctx = canvas.getContext('2d');
-
-    canvas.width = img.width;
-    canvas.height = img.height;
-    ctx.drawImage(img, 0, 0, img.width, img.height);
-
-    const predictions = await model.detect(canvas);
-
-}
